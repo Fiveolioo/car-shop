e8b1cdf9bd6889f1361d892cf35d9f890f76d12e
Fiveolioo <96richardohc@gmail.com>
2020-10-17 02:07:46 -1000

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'users#show'
+  resources :users, only: [:show] do
+    resources :cars
+end
+
+  resources :users, only: [:show] do
+    resources :appointments
+end
+
+  resources :shops
 end
