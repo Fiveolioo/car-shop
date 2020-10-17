37d66d77fcdb96011997644a7195d75bbc14bc10
Fiveolioo <96richardohc@gmail.com>
2020-10-17 01:58:28 -1000

    added associations

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,2 +1,5 @@
 class Appointment < ApplicationRecord
+    belongs_to :user
+    belongs_to :shop
+    belongs_to :car
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_many :cars
+    has_many :appointments
+    has_many :shops, through: :appointments
+    accepts_nested_attributes_for :cars
+    accepts_nested_attributes_for :appointments
 end
