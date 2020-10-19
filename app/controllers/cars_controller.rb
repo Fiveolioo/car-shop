0dd7c468525eb04af354c099f4d3b5bde71c83d0
Fiveolioo <96richardohc@gmail.com>
2020-10-18 18:11:38 -1000

    finished cars#new

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,35 +1,37 @@
 class CarsController < ApplicationController
     
     def index
-        if params[:user_id]
-            @cars = User.find(params[:user_id]).cars
-            @user = User.find(params[:user_id])
-        else
-            redirect_to user_cars_path
-        end
+      if params[:user_id]
+        @cars = User.find(params[:user_id]).cars
+        @user = User.find(params[:user_id])
+      else
+        redirect_to user_cars_path
+      end
     end
 
    
     def new
       if params[:user_id] && !User.exists?(params[:user_id])
-      flash[:notice] = 'User not found.'
-      redirect_to new_user_registration_path
+        flash[:notice] = 'User not found.'
+        redirect_to new_user_registration_path
       else
-      @car = Car.new(user_id: params[:user_id])
+        @car = Car.new(user_id: params[:user_id])
       end
     end
 
     def create
-      @car = Car.new(pet_params)
+      @car = Car.new(car_params)
       if @car.save
-       @user = current_user
-       redirect_to user_car_path(@user, @car)
+         @user = current_user
+         redirect_to user_car_path(@user, @car)
       else
        render :new
       end
     end
 
     def show
+      @car = car
+      @user = current_user
     end
 
     def edit
@@ -44,7 +46,7 @@ class CarsController < ApplicationController
     private
 
     def car_params
-      params.require(:car).permit(:make, :model, :color)
+      params.require(:car).permit(:make, :model, :color, :user_id)
     end
   
     def car
