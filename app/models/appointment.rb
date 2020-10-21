2da94bf39a594c2db5c1f03ebe4d0a622129e589
Fiveolioo <96richardohc@gmail.com>
2020-10-20 21:25:00 -1000

    finished appts validations

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,9 +1,22 @@
 class Appointment < ApplicationRecord
+    validates :date_time, presence: true
+    validate :validate_appt
+    validates :reason_for_visit, presence: true
+    validates :shop, presence: true
+
     belongs_to :user
     belongs_to :shop
     belongs_to :car
 
     def self.reasons
         ['Repair', 'Paint', 'Installation', 'Alignment', 'Tire Change', 'Maintenance', 'Diagnostics']
+    end
+
+    private
+
+    def validate_appt
+      if date_time < Date.today
+        errors.add(:date_time, 'Invalid')
+      end
     end
 end
