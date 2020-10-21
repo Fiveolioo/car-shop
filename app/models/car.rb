9a7f2b0d059b8b0e6569e99b07a4f9662ff0721b
Fiveolioo <96richardohc@gmail.com>
2020-10-20 21:08:25 -1000

    created CSS and validation update

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,7 +2,7 @@ class Car < ApplicationRecord
     validates :make, presence: true
     validates :model, presence: true
     validates :color, presence: true
-    validates :nickname, presence: true
+    validates :nickname, presence: true, uniqueness: true
 
     belongs_to :user
     has_many :appointments, through: :user
